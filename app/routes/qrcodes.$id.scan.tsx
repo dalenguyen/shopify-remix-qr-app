0d56b8d8ee6bd299909b0a798e7654e8cebc8d14
Dale Nguyen <dungnq@itbox4vn.com>
2024-02-27 23:32:53 -0500

    redirect customer to the destination URL

---
+++ app/routes/qrcodes.$id.scan.tsx
@@ -0,0 +1,20 @@
+import invariant from "tiny-invariant";
+import db from "../db.server";
+import { redirect } from "@remix-run/node";
+import { getDestinationUrl } from "~/models/QRcode.server";
+
+export const loader = async ({ params }) => {
+  invariant(params.id, `Could not find QR code destination`);
+
+  const id = Number(params.id);
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
+
+  invariant(qrCode, `Could not find QR code destination`);
+
+  await db.qRCode.update({
+    where: { id },
+    data: { scans: { increment: 1 } },
+  });
+
+  return redirect(getDestinationUrl(qrCode));
+};
