f4db8a6742b91e5ba099c817359675ed6ca40285
Dale Nguyen <dungnq@itbox4vn.com>
2024-03-04 23:06:16 -0500

    feat(app): add bulky operation page

---
+++ app/routes/app.dashboard.tsx
@@ -0,0 +1,10 @@
+import { Layout, Page } from "@shopify/polaris";
+
+export default function DashboardPage() {
+  return (
+    <Page>
+      <ui-title-bar title="Dashboard Page" />
+      <Layout></Layout>
+    </Page>
+  );
+}
